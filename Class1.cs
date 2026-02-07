b70a58472a07d607c78fa69323915c64d56e4e31
Manoranjan Lenka <manoranjanlenka314@gmail.com>
2026-02-07 23:28:34 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;   // Needed for ArrayList
+namespace Collection
+{
+    class StudentListProgram
+    {
+        static void Main()
+        {
+            // Create an ArrayList to store student names
+            ArrayList students = new ArrayList();
+
+            // Accept 5 student names from the user
+            Console.WriteLine("Enter 5 student names:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter name " + (i + 1) + ": ");
+                string name = Console.ReadLine();
+                students.Add(name);
+            }
+
+            // Display all student names entered
+            Console.WriteLine("\nList of Students:");
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+            }
+
+            // Ask the user to enter a name to search
+            Console.Write("\nEnter a name to search: ");
+            string searchName = Console.ReadLine();
+
+            int index = students.IndexOf(searchName);
+            if (index != -1)
+            {
+                Console.WriteLine($"Student {searchName} found at index {index}");
+            }
+            else
+            {
+                Console.WriteLine($"Student {searchName} not found");
+            }
+
+            // Ask the user to enter a name to remove
+            Console.Write("\nEnter a name to remove: ");
+            string removeName = Console.ReadLine();
+
+            if (students.Contains(removeName))
+            {
+                students.Remove(removeName);
+                Console.WriteLine($"{removeName} removed successfully!");
+            }
+            else
+            {
+                Console.WriteLine($"{removeName} not found in the list!");
+            }
+
+            // Display the updated list
+            Console.WriteLine("\nUpdated Student List:");
+            foreach (string student in students)
+            {
+                Console.WriteLine(student);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Collection
+{
+    // Online C# Editor for free
+    // Write, Edit and Run your C# code using C# Online Compiler
+
+    public class Student
+    {
+        public static void Main()
+        {
+            Dictionary<int, string> emp = new Dictionary<int, string>();
+            emp.Add(1, "Name: sam ,Fee:5000");
+            emp.Add(2, "Name: John,Fee:8000");
+            emp.Add(3, "Name: Kiran,Fee:6000");
+            foreach (var i in emp)
+            {
+                Console.WriteLine($"ID: {i.Key}, {i.Value}");
+            }
+
+        }
+    }
+}
