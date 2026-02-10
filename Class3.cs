b7af18e1888d2d72ad9d723aa0372c96e51d88bf
Manoranjan Lenka <manoranjanlenka314@gmail.com>
2026-02-10 23:48:22 +0530

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace Interface
+{
+    public class Calculato
+
+    {
+        public int AddingNewEventArgs(int a, int b)
+        {
+          return a + b;
+        }
+        public int AddingNewEventArgs(int a, int b,int c)
+        {
+            return a + b + c;
+        }
+
+        static void Main()
+        {
+            Calculato m1=new Calculato();
+            Console.WriteLine("add " +  m1.AddingNewEventArgs(1,2));
+            Console.WriteLine("add " +  m1.AddingNewEventArgs(1, 2,3));
+        }
+
+    }
+}
