1f227eac5497fda7ddd9f03c9975e3a5759d2fdd
Manoranjan Lenka <manoranjanlenka314@gmail.com>
2026-02-19 23:38:41 +0530

    Add files via upload

---
+++ Class7.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSproject
+{
+    public  class Leela
+    {
+        public virtual void display()
+        {
+            Console.WriteLine("I am Leela from leela class.");
+        }
+    }
+    public class Hemon : Leela
+    {
+        public void show()
+        {
+            Console.WriteLine("I am Hemon from Hemon class.");
+
+        }
+        public override void display()
+        {
+            Console.WriteLine("I am overridden display method from Hemon class.");
+        }
+        public static void Main(string[] args)
+        {
+            Hemon h = new Hemon();
+            h.show();
+            h.display();
+            Leela l = new Leela();
+            l.display();
+            Leela le = new Hemon();
+            le.display();
+        }
+
+    }
+}
